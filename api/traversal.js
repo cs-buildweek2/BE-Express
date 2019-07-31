231af288622cf72467e17dfd88eb065090fabb83
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-31 13:12:56 -0400

    Built out the traversal algorithm for continuing until you hit a dead-end. Commented out version of the algorithm which logs new graph entries into the database as it goes (will switch main algorithm to do this all at once when it finishes for cleaner code).

--- api/traversal.js
+++ api/traversal.js
@@ -5,7 +5,7 @@ const Exits = require("../data/helpers/exits-model.js");
 const roomRequest = async (token, direction) => {
   const URL = "https://lambda-treasure-hunt.herokuapp.com/api/adv/move/";
   const headers = {
-    Authorization: `Token ${secret}`,
+    Authorization: `Token ${token}`,
     "Content-Type": "application/json"
   };
   const request = { direction };
@@ -26,32 +26,142 @@ const initialization = async token => {
 const traversal = async token => {
   const graph = {};
   const startingRoom = initialization(token);
-  const newRoom = {
-    room_id: startingRoom.room_id,
-    title: startingRoom.title,
-    description: startingRoom.description
-  };
-  graph[startingRoom] = {};
-  await Rooms.create(newRoom);
+  graph[startingRoom.room_id] = {};
+  // await Rooms.create(newRoom);
   if (startingRoom.exits.length > 0) {
-    for (exit of exits) {
-      const newExit = {
-        room_id: startingRoom.id,
-        direction: exit
-      };
-      await Exits.create(newExit);
-      graph[startingRoom][exit] = "?";
+    for (let exit of startingRoom.exits) {
+      graph[startingRoom.room_id][exit] = "?";
     }
   }
   const totalRooms = 500;
   const visited = new Set();
   const s = [];
   // will hold rooms here that have unexplored neighbors, for the purpose of backtracking
+  let startingTime = new Date().getTime() / 1000;
+  let cooldown = startingRoom.cooldown;
   visited.add(startingRoom.room_id);
+  s.push(startingRoom.room_id);
+  let currentRoom = startingRoom.room_id;
+  let backtracking = false;
   while (visited.size < totalRooms) {
-    return;
-    // Traverse until you hit a dead-end, then back-track to the nearest room with an unexplored path.
+    let currentTime = new Date().getTime() / 1000;
+    if (currentTime - startingTime >= cooldown) {
+      if (!backtracking) {
+        let explored = true;
+        for (let direction in graph[currentRoom]) {
+          if (graph[currentRoom][direction] === "?") {
+            explored = false;
+            const movedToRoom = roomRequest(token, direction);
+            const newRoomID = movedToRoom.room_id;
+            // Reset cooldown and starting time after making a move
+            cooldown = movedToRoom.cooldown;
+            startingTime = new Date().getTime() / 1000;
+            graph[newRoomID] = {};
+            visited.add(newRoomID);
+            if (movedToRoom.exits.length > 0) {
+              for (let exit of movedToRoom.exits) {
+                graph[newRoomID][exit] = "?";
+                s.push(newRoomID);
+              }
+            } else {
+              backtracking = true;
+            }
+            currentRoom = newRoomID;
+            break;
+          }
+        }
+
+        if (explored) {
+          backtracking = true;
+        }
+      } else {
+        // Backtracking goes here
+      }
+    }
   }
 };
 
 module.exports = traversal;
+
+// Commenting out the version which adds new entries as it goes
+// const traversal = async token => {
+//   const graph = {};
+//   const startingRoom = initialization(token);
+//   // const newRoom = {
+//   //   room_id: startingRoom.room_id,
+//   //   title: startingRoom.title,
+//   //   description: startingRoom.description
+//   // };
+//   graph[startingRoom.room_id] = {};
+//   // await Rooms.create(newRoom);
+//   if (startingRoom.exits.length > 0) {
+//     for (let exit of startingRoom.exits) {
+//       // const newExit = {
+//       //   room_id: startingRoom.room_id,
+//       //   direction: exit
+//       // };
+//       // await Exits.create(newExit);
+//       graph[startingRoom.room_id][exit] = "?";
+//     }
+//   }
+//   const totalRooms = 500;
+//   const visited = new Set();
+//   const s = [];
+//   // will hold rooms here that have unexplored neighbors, for the purpose of backtracking
+//   let startingTime = new Date().getTime() / 1000;
+//   let cooldown = startingRoom.cooldown;
+//   visited.add(startingRoom.room_id);
+//   s.push(startingRoom.room_id);
+//   let currentRoom = startingRoom.room_id;
+//   let backtracking = false;
+//   while (visited.size < totalRooms) {
+//     let currentTime = new Date().getTime() / 1000;
+//     if (currentTime - startingTime >= cooldown) {
+//       let explored = true;
+//       for (let direction in graph[currentRoom]) {
+//         if (graph[currentRoom][direction] === "?") {
+//           explored = false;
+//           const movedToRoom = roomRequest(token, direction);
+//           const newRoomID = movedToRoom.room_id;
+//           // Reset cooldown and starting time after making a move
+//           cooldown = movedToRoom.cooldown;
+//           startingTime = new Date().getTime() / 1000;
+//           // const createMovedToRoom = {
+//           //   room_id: movedToRoom.room_id,
+//           //   title: movedToRoom.title,
+//           //   description: movedToRoom.description
+//           // };
+//           // await Rooms.create(createMovedToRoom);
+//           graph[newRoomID] = {};
+//           visited.add(newRoomID);
+//           // const previousExit = await Exits.findByRoom(currentRoom, direction);
+//           // const newPreviousExit = {
+//           //   direction_id: newRoomID
+//           // };
+//           // await Exits.update(newPreviousExit, previousExit.id);
+//           if (movedToRoom.exits.length > 0) {
+//             for (let exit of movedToRoom.exits) {
+//               // const newExit = {
+//               //   room_id: newRoomID,
+//               //   direction: exit
+//               // };
+//               // await Exits.create(newExit);
+//               // It will probably look cleaner to make all the database entries for the graph at once, once it is completely built
+//               graph[newRoomID][exit] = "?";
+//               s.push(newRoomID);
+//             }
+//           } else {
+//             backtracking = true;
+//           }
+//           break;
+//         }
+//       }
+
+//       if (explored) {
+//         backtracking = true;
+//       }
+//     }
+//   }
+// };
+
+// module.exports = traversal;
