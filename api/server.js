ffe7d4ff07f3b8e9feb69b984e8f6857116320e8
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-31 00:43:59 -0400

    Axios added as a dependency. Added server routes for checking if API is working, and for activating a traversal loop.

--- api/server.js
+++ api/server.js
@@ -8,4 +8,12 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.get("/", (req, res) => {
+  res.send("Api is up and running");
+});
+
+server.post("/traverse", (req, res) => {
+  res.send("Traversal will go here");
+});
+
 module.exports = server;
